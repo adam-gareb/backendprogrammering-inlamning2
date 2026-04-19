0f9831d4296744f366b4e5636e4ee381ad2b5d06
Adam <adamgareb@hotmail.com>
2026-04-19 23:16:23 +0200

    Done with Del 2

---
+++ src/main/java/se/yrgo/services/calls/CallHandlingServiceImpl.java
@@ -0,0 +1,30 @@
+package se.yrgo.services.calls;
+
+import se.yrgo.domain.Action;
+import se.yrgo.domain.Call;
+import se.yrgo.domain.Customer;
+import se.yrgo.services.customers.CustomerManagementService;
+import se.yrgo.services.customers.CustomerNotFoundException;
+import se.yrgo.services.diary.DiaryManagementService;
+
+import java.util.Collection;
+
+public class CallHandlingServiceImpl implements CallHandlingService {
+
+    private CustomerManagementService customerService;
+    private DiaryManagementService diaryService;
+
+    public CallHandlingServiceImpl(CustomerManagementService customerService, DiaryManagementService diaryService) {
+        this.customerService = customerService;
+        this.diaryService = diaryService;
+    }
+
+    @Override
+    public void recordCall(String customerId, Call newCall, Collection<Action> actions) throws CustomerNotFoundException {
+        customerService.recordCall(customerId, newCall);
+
+        for (Action action : actions){
+            diaryService.recordAction(action);
+        }
+    }
+}
--- src/main/java/se/yrgo/services/diary/DiaryManagementServiceMockImpl.java
+++ src/main/java/se/yrgo/services/diary/DiaryManagementServiceMockImpl.java
@@ -1,5 +1,6 @@
 package se.yrgo.services.diary;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -8,20 +9,24 @@ import se.yrgo.domain.Action;
 
 public class DiaryManagementServiceMockImpl implements DiaryManagementService {
 	
-	private Set<Action>allActions= new HashSet<Action>();
+	private Set<Action> allActions = new HashSet<Action>();
 
 	@Override
 	public void recordAction(Action action) {
-		// TODO Auto-generated method stub
-
+		allActions.add(action);
 	}
 
 	//Hint: 
 	//Create a list<Action>
 	//In the for each loop going through the list use this condition: "if(action.getOwningUser().equals(requiredUser) && !action.isComplete())" to add a new action to the list. 
 	public List<Action> getAllIncompleteActions(String requiredUser) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Action> result = new ArrayList<>();
+		for(Action action : allActions){
+			if(action.getOwningUser().equals(requiredUser) && !action.isComplete()){
+				result.add(action);
+			}
+		}
+		return result;
 	}
 
 }
